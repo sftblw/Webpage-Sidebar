fb5c90daabf1454747ae140692491d687462e500
Ch. (Chanhwi, Choi) <ccwpc@hanmail.net>
2017-08-16 13:46:44 +0900

    initial complete implementation

--- launcher/launcher.js
+++ launcher/launcher.js
@@ -1,9 +1,37 @@
 'use strict';
 
+// initialize list
+
+browser.runtime.sendMessage({kind: 'storage.sites.get'}).then( (message) => {
+  console.log(message);
+  let ul = document.querySelector('#sites ul');
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  if (message !== undefined) {
+    for (let site of message) {
+      addSiteElem(site.url);
+    }
+  }
+});
+
+
 // ref: https://github.com/mdn/webextensions-examples/blob/master/beastify/popup/choose_beast.js
 document.addEventListener('click', (ev) => {
   if (ev.target.classList.contains('openSite')) {
     openSite(ev);
+  }
+  else if (ev.target.classList.contains('addSite')) {
+    addSite(ev);
+  }
+  else if (ev.target.classList.contains('removeSite')) {
+    removeSite(ev);
+  }
+});
+
+document.addEventListener('submit', (ev) => {
+  if (ev.target.classList.contains('addSite')) {
+    addSite(ev);
   }
 });
 
@@ -11,7 +39,57 @@ function openSite (ev) {
   ev.preventDefault();
   browser.sidebarAction.getPanel({})
     .then( (panel) => { 
-      console.dir(panel);
       browser.sidebarAction.setPanel({panel: ev.target.href});
     });
+}
+
+function addSite (ev) {
+  ev.preventDefault();
+
+  let url;
+  
+  {
+    let urlElem;
+
+    if (ev.target.nodeName.toLowerCase() === 'form') {
+      urlElem = ev.target.querySelector('.url');
+      url = urlElem.value;
+      ev.target.querySelector('.url').value = '';
+    } else if (ev.target.nodeName.toLowerCase() === 'input') {
+      urlElem = ev.target.parentNode.querySelector('.url');
+      url = urlElem.value;
+    } else {
+      console.error(`unknown event source ${ev.target.nodeName} for addSite(). aborting.`);
+      return;
+    }
+    urlElem.value = '';
+  }
+
+  browser.runtime.sendMessage({kind: 'storage.sites.add', site: {url}}).then(handleOk);
+  addSiteElem(url);
+}
+
+function addSiteElem(url) {
+  let template = `<a href="${url}" class="openSite">${url}</a>
+  <input type="button" value="X" class="removeSite"/>`;
+
+  let li_elem = document.createElement('li');
+  li_elem.innerHTML = template;
+
+  document.querySelector('#sites ul').appendChild(li_elem);
+}
+
+function removeSite (ev) {
+  ev.preventDefault();
+  
+  ev.target.parentNode.parentNode.removeChild(ev.target.parentNode);
+  browser.runtime.sendMessage({kind: 'storage.sites.remove', site: {
+    'url': ev.target.parentNode.querySelector('.openSite').href
+  }}).then(handleOk);
+}
+
+function handleOk(response) {
+  if (response !== 'ok') {
+    console.error('background script did not send OK');
+  }
 }
---
+++ launcher/storage.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let sites = [
+  // {'url': 'https://mobile.twitter.com/'}
+];
+
+getInitialStorage();
+// browser.storage.sync.clear();
+
+/// handle request from panel
+browser.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+  if (request.kind === 'storage.sites.get') {
+    sendResponse(sites);
+  }
+  else if (request.kind === 'storage.sites.add') {
+    sites.push(request.site);
+    browser.storage.sync.set({sites});
+    sendResponse('ok');
+  }
+  else if (request.kind === 'storage.sites.remove') {
+    sites = sites.filter( (value, index) => {
+      value.url != request.site.url
+    });
+    browser.storage.sync.set({sites});
+    sendResponse('ok');
+  }
+});
+
+
+function getInitialStorage () {
+  browser.storage.sync.get('sites').then( (storage_value) => {
+    if (storage_value.sites === undefined) {
+      browser.storage.sync.set({'sites': []});
+      sites = [];
+    } else {
+      sites = storage_value.sites;
+    }
+  });
+}
